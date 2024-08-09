8d47dac1953b75daf62dbab7cc2697f111b187c2
Charoanchai <117244839+Armm007@users.noreply.github.com>
2024-08-09 09:45:52 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function calculate() {  
+        var num1 = document.getElementById("num1").value;
+        var num2 = document.getElementById("num2").value;
+
+        var result = Number(num1) + Number(num2);
+        document.getElementById("result").innerHTML = result;
+
+}
